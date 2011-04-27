f01edc69a27504995846a1620bda0a6ed8d153a0
d2x-xl <karx11erx@hotmail.com>
2011-04-27 01:08:06 +0000

    Fixed: Custom textures sometimes were shown upside down Fixed: The texture tools texture view displayed textures upside down

--- textures.cpp
+++ textures.cpp
@@ -103,12 +103,12 @@ if (bShowTexture) {
 			{
 			double scale = tex.Scale ();
 			if (scale == 1.0) {
-				for (uint i = 0, y = 0; y < tex.Width (); y++)
+				for (uint i = 0, y = tex.Width (); y > 0; y--)
 					for (uint x = 0; x < tex.Width (); x++)
 						pDC->SetPixel ((int) x, (int) y, textureManager.m_bmBuf [i++].ColorRef ());
 				}
 			else {
-				for (uint y = 0; y < tex.Width (); y = (int) (y + scale))
+				for (uint y = tex.Width (); y > 0; y = (int) (y - scale))
 					for (uint x = 0; x < tex.Width (); x = (int) (x + scale))
 						pDC->SetPixel ((int) (x / scale), (int) (y / scale), textureManager.m_bmBuf [y * tex.Width () + x].ColorRef ());
 				}
